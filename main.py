0071b638bdac91130cb1accb5c63d0e6f698cfa8
YifengQ <yifengqin88@gmail.com>
2020-10-12 00:46:50 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,89 @@
+__author__ = "Yifeng Qin"
+__class__ = "CS457 Database Management Systems"
+__instructor__ = "Dongfang Zhao"
+__university__ = "University of Nevada Reno"
+__assignment__ = "Project 1"
+__date__ = "9/15/2020"
+
+import sys
+from run_script import RunScript
+
+# initializes the script and file classes
+script = RunScript()
+
+
+def read_file():
+    """
+    Opens the file with standard input, and reads every line and checks if it is a valid command to be
+    appended to the list
+    :return: Returns a List of the Commands
+    """
+    commands = []
+    for line in sys.stdin:
+        if line == '\r\n' or line[0:2] == '--':  # checks if it is a empty new line or it starts with '--'
+            continue
+        else:
+            commands.append(line.rstrip())  # removes newline and special characters from line to append to list
+
+    return commands
+
+
+def run_commands():
+    """
+    Calls the helper function read_file to get all the valid commands from the file. It will run through the list and
+    splice the commands to get rid of special characters.
+    :return: None
+    """
+    commands = read_file()  # calls helper function to initialize to list commands
+    for command in commands:
+        l = command.split(' ')  # splits the string command into a list based on spaces
+        command = command.upper()  # converts the command to all uppercase so it can cover case sensitivity
+        size = len(l)  # gets length to handle missing spaces
+        if 'CREATE DATABASE' in command:
+            if size == 3:  # checks if all arguments are present
+                script.create_database(l[2][:-1])  # only gets the database name and removes the ';' from the back
+            else:
+                print('Syntax Error:', command)  # if size does not match there has to be a syntax error with cmd
+        elif 'DROP DATABASE' in command:
+            if size == 3:  # checks if all arguments are present
+                script.drop_database(l[2][:-1])   # only gets the database name and removes the ';' from the back
+            else:
+                print('Syntax Error:', command)  # if size does not match there has to be a syntax error with cmd
+        elif 'DROP TABLE' in command:
+            if size == 3:  # checks if all arguments are present
+                script.drop_table(l[2][:-1])   # only gets the database name and removes the ';' from the back
+            else:
+                print('Syntax Error:', command)  # if size does not match there has to be a syntax error with cmd
+        elif 'USE' in command:
+            if size == 2:  # checks if all arguments are present
+                script.use_database(l[1][:-1])   # only gets the database name and removes the ';' from the back
+            else:
+                print('Syntax Error:', command)  # if size does not match there has to be a syntax error with cmd
+        elif 'CREATE TABLE' in command:
+            if size >= 3:  # checks the the minimum amount of arguments are present
+                command = " ".join(l[3:])  # gets all the variables after the table name and converts it into a string
+                command = command[1:-2]  # then slice off the beginning '(' and the ');' at the end
+                script.create_table(l[2], command)  # passes in the name of the table and the sliced variables to input
+            else:
+                print('Syntax Error:', command)  # if size does not match there has to be a syntax error with cmd
+        elif 'SELECT * FROM' in command:
+            if size == 4:  # checks if all arguments are present
+                script.select_all(l[3][:-1]) # only gets the table name and removes the ';' from the back
+            else:
+                print('Syntax Error:', command)  # if size does not match there has to be a syntax error with cmd
+        elif 'ALTER TABLE' in command:
+            if size >= 4:  # checks if all arguments are present
+                command = " ".join(l[4:])  # gets all the variables after the table name and converts it into a string
+                command = command[:-1]  # removes the ';' from the back of string
+                script.alter_table(l[2], command)  # passes in the name of table and sting of variables
+            else:
+                print('Syntax Error:', command)  # if size does not match there has to be a syntax error with cmd
+        elif '.EXIT' in command:
+            return
+        else:  # if the command is not recognised it's and unknown command or there is something wring with the syntax
+            print('Syntax Error | Unknown Command')
+            print(command)
+
+
+run_commands()
+
---
+++ run_script.py
@@ -0,0 +1,147 @@
+__author__ = "Yifeng Qin"
+__class__ = "CS457 Database Management Systems"
+__instructor__ = "Dongfang Zhao"
+__university__ = "University of Nevada Reno"
+__assignment__ = "Project 1"
+__date__ = "9/15/2020"
+
+import os
+
+
+class RunScript:
+
+    def __init__(self):
+        """
+        Gets the current working directory and initializes variables
+        """
+        self.parentDir = os.getcwd()
+        self.dbDir = None
+        self.data = []
+
+    def create_database(self, db):
+        """
+        Joins the parent directory with the entered database name. Checks if the database already exists. If it exists
+        already it will print out and error, if not it will create a directory of the database name.
+        :param db: string that contains the name of the database
+        :return: None
+        """
+        path = os.path.join(self.parentDir, db)  # joins cwd and db name
+        if os.path.exists(path):  # check if path exists
+            output = '!Failed to create database ' + db + ' because it already exists'
+            print(output)
+        else:
+            os.mkdir(path)  # creates directory of path
+            output = 'Database ' + db + ' created.'
+            print(output)
+
+    def drop_database(self, db):
+        """
+        Joins the parent directory with the entered database. Checks if the database already exists, and will either
+        error out or delete that database.
+        :param db: string that contains the name of the database
+        :return: None
+        """
+        path = os.path.join(self.parentDir, db)  # check if path exists
+        if os.path.exists(path):   # check if path exists
+            cmd = 'rm ' + '-rf ' + path  # concatenate command to input
+            os.system(cmd)  # runs the command
+            output = 'Database ' + db + ' deleted.'
+            print(output)
+        else:
+            output = '!Failed to delete ' + db + ' because it already exists.'
+            print(output)
+
+    def use_database(self, db):
+        """
+        Joins the parent directory with the entered database. Checks if the database already exists, and will either
+        error out change the working directory to the database.
+        :param db: string that contains the name of the database
+        :return: None
+        """
+        path = os.path.join(self.parentDir, db)  # joins cwd and db name
+        if os.path.exists(path):  # check if path exists
+            os.chdir(path)  # changes cwd to this path
+            self.dbDir = path
+            output = 'Using database ' + db + '.'
+            print(output)
+        else:
+            print('Cannot Use Database | Does Not Exist')
+
+    def create_table(self, tbl, inp):
+        """
+        Gets the path to that table and will check if that already exists. Then if it does not exist, it will change the
+        directory and call a helper function to append data to the table.
+        :param tbl:
+        :param inp: Contains all the data that will be entered into the table
+        :return: None
+        """
+        path = os.path.join(self.dbDir, tbl)  # joins cwd and db name
+        if os.path.exists(path):   # check if path exists
+            output = '!Failed to create table ' + tbl + ' because it already exists.'
+            print(output)
+        else:
+            os.mknod(path)  # creates file system of path
+            out = inp.split(',')
+            out = "|".join(out)
+            f = open(path, "a")  # opens file
+            f.write(out)  # write to file
+            f.close()  # close file
+            output = 'Table ' + tbl + ' created.'
+            print(output)
+
+    def select_all(self, table):
+        """
+        Checks if the table exists and then reads all the data from the file and prints it out.
+        :param table: String that contains name of the table
+        :return:
+        """
+        path = os.path.join(self.dbDir, table)  # joins cwd and db name
+        if os.path.exists(path):  # check if path exists
+            with open(path) as file_in:  # starts reading from file
+                for line in file_in:
+                    self.data.append(line.rstrip())
+            for line in self.data:  # prints data to terminal
+                print(line)
+            self.data = []
+        else:
+            output = '!Failed to query table ' + table + ' because it does not exist.'
+            print(output)
+
+    def alter_table(self, tbl, inp):
+        """
+        Will check if the table exists, if it doesn't exist it will print out an error.
+        If it exists it will then append the extra values to the file.
+        :param tbl: String containing name of table
+        :param inp: string that need to be inputted
+        :return: None
+        """
+        path = os.path.join(self.dbDir, tbl)  # joins cwd and db name
+        if os.path.exists(path):  # check if path exists
+            out = inp.split(',')  # takes the string a separates all the values by comma's and storing it into a list
+            out = "|".join(out)  # joins the list back together into a string with a '|' at value
+            f = open(path, "a")  # opens file
+            f.write('| ' + out)  # adds '|' to separate existing values and then writes the output string
+            f.close()  # close file
+            output = 'Table ' + tbl + ' modified.'
+            print(output)
+        else:
+            output = '!Failed to alter table ' + tbl + ' because it does not exist'
+            print(output)
+
+    def drop_table(self, tbl):
+        """
+        Checks if the table exists and if that table exists it will delete that path. If it does not exist
+        it will error and print the error message
+        :param tbl: string that contains the name of the table
+        :return: None
+        """
+        path = os.path.join(self.dbDir, tbl)  # check if path exists
+        if os.path.exists(path):  # check if path exists
+            cmd = 'rm ' + '-rf ' + path  # concatenate command to run
+            os.system(cmd)  # runs the command
+            output = 'Table ' + tbl + ' deleted.'
+            print(output)
+        else:
+            output = '!Failed to delete ' + tbl + ' because it does not exists.'
+            print(output)
+
